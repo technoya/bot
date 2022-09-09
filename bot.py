e11f4a0f654bf78ce28d93e93a1dde72162dfdba
technoya <113179663+technoya@users.noreply.github.com>
2022-09-09 11:34:25 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,112 +1,68 @@
-import logging
-from dotenv import load_dotenv
-from telegram.ext.filters import Filters
-from telegram import ParseMode
-import os
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
-from telegram import Update, replymarkup
-from telegram.ext import (Updater,
-                          PicklePersistence,
-                          CommandHandler,
-                          CallbackQueryHandler,
-                          CallbackContext,
-                          ConversationHandler)
-from telegram import InlineKeyboardButton as IKB, InlineKeyboardMarkup, ForceReply
+from telegram.ext.filters import Filters
 
-
-load_dotenv()
-BOT_TOKEN = os.getenv('5721627024:AAHtBmuJMMtKtM-JTo3VecF9jYvS1_S0_N8')
+updater = Updater("your_own_API_Token got from BotFather",
+				use_context=True)
 
 
 def start(update: Update, context: CallbackContext):
-    ''' Replies to start command '''
-    update.message.reply_text('Hi! I am alive. Click /post to create a post')
+	update.message.reply_text(
+		"Hello sir, Welcome to the Bot.Please write\
+		/help to see the commands available.")
+
+def help(update: Update, context: CallbackContext):
+	update.message.reply_text("""Available Commands :-
+	/youtube - To get the youtube URL
+	/linkedin - To get the LinkedIn profile URL
+	/gmail - To get gmail URL
+	/geeks - To get the GeeksforGeeks URL""")
 
 
-def main_menu(update: Update, context: CallbackContext):
-    ''' Entry point of conversation  this gives  buttons to user'''
-
-    update.message.reply_text('''Choose your option:\n
-    /add : to add a button
-    /preview : to preview current post
-    /send : to send current post
-    /cancel : to delete current post
-    ''',)
+def gmail_url(update: Update, context: CallbackContext):
+	update.message.reply_text(
+	"BOT UNDER MAINTENCE !")
 
 
-def add_button(update: Update, context: CallbackContext):
-    args = ' '.join(context.args).strip()
-    if not args:
-        update.message.reply_text(
-            'Please send the text and link with the command. \nExample: \n\n`/add Click here - https://aahnik.github.io`',parse_mode= ParseMode.MARKDOWN)
-        return
-    try:
-        splitted = args.split('-')
-        print(splitted)
-        text = splitted[0].strip()
-        url = splitted[1].strip()
-        print(text,url)
-    except Exception as err:
-        update.message.reply_text(str(err)[:2000])
-        return
-
-    user_d = context.user_data
-    if not 'buttons' in user_d:
-        user_d['buttons'] = []
-    user_d['buttons'].append([IKB(text, url=url)])
-    update.message.reply_text('Done')
-    main_menu(update,context)
+def youtube_url(update: Update, context: CallbackContext):
+	update.message.reply_text("BOT UNDER MAINTENCE !")
 
 
-def preview(update: Update, context: CallbackContext):
-    user_d = context.user_data
-    buttons = user_d.get('buttons')
-    if buttons:
-        update.message.reply_text(
-            'Buttons Preview', reply_markup=InlineKeyboardMarkup(buttons))
-    else:
-        update.message.reply_text('No buttons added yet')
-    main_menu(update,context)
-
-def send(update: Update, context: CallbackContext):
-    update.message.reply_text('Sent message')
-    #  send the message
-
-    user_d = context.user_data
-    user_d.clear()
+def linkedIn_url(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"LinkedIn URL => \
+		https://www.linkedin.com/in/dwaipayan-bandyopadhyay-007a/")
 
 
-def cancel(update: Update, context: CallbackContext):
-    user_d = context.user_data
-    user_d.clear()
-    update.message.reply_text('Cleared user data')
-    main_menu(update,context)
+def geeks_url(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"BOT UNDER MAINTENCE !!")
 
 
-if __name__ == "__main__":
+def unknown(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Sorry '%s' is not a valid command" % update.message.text)
 
-    updater = Updater(token=BOT_TOKEN)
 
-    dispatcher = updater.dispatcher
+def unknown_text(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Sorry I can't recognize you , you said '%s'" % update.message.text)
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
 
-    _handlers = {}
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
+updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
+updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+updater.dispatcher.add_handler(MessageHandler(
+	Filters.command, unknown)) # Filters out unknown commands
 
-    _handlers['start_handler'] = CommandHandler('start', start)
+# Filters out unknown messages.
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
 
-    _handlers['post_hanlder'] = CommandHandler('post', main_menu)
-
-    _handlers['add_button_handler'] = CommandHandler('add', add_button)
-    _handlers['preview_handler'] = CommandHandler('preview', preview)
-    _handlers['cancel_handler'] = CommandHandler('cancel', cancel)
-    _handlers['send_handler'] = CommandHandler('send', send)
-
-    for name, _handler in _handlers.items():
-        print(f'Adding handler {name}')
-        dispatcher.add_handler(_handler)
-
-    updater.start_polling()
-
-    updater.idle()
+updater.start_polling()
