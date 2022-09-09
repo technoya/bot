9e6be10ed2f56b9d10959e1e709813c262f14ec0
technoya <113179663+technoya@users.noreply.github.com>
2022-09-09 11:06:27 -0700

    Update botm.py

--- botm.py
+++ botm.py
@@ -1,14 +1,48 @@
-import requests
-import time
 
-# list of quotes
-quotes = [
-  'BOT UNDER MAINTENCE WAIT !!'
-]
+import os
+import logging
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# loop through the quotes
-for quote in quotes:
-    url = 'https://api.telegram.org/bot5721627024:AAHtBmuJMMtKtM-JTo3VecF9jYvS1_S0_N8/sendMessage?chat_id=CHAT_ID&text="{}"'.format(quote)
-    requests.get(url)
-    # sends new quotes every 20seconds
-    time.sleep(2000000)
+Bot = Client(
+    "Maintain-Bot",
+    bot_token=os.environ["5721627024:AAHtBmuJMMtKtM-JTo3VecF9jYvS1_S0_N8"],
+    api_id=int(os.environ["18680464"]),
+    api_hash=os.environ["e5c7c5e22094ecdeee4275f5764af1cc"],
+)
+
+updatesc = os.environ["UPDATES_CHANNEL"]
+supportc = os.environ["SUPPORT_CHAT"]
+btname = os.environ["BOT_NAME"]
+
+BOT_TEXT = """
+Hai {} , This Bot Is Under Maintenance.
+
+You Can't Use This Bot Right Now.You Will Get a Message On This Bot's Channel If This Bot Is Ready To Work.
+"""
+
+BOT_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="Channel", url=f"https://telegram.me/{updatesc}"),
+            InlineKeyboardButton(text="Support", url=f"https://telegram.me/{supportc}"),
+        ]
+    ]
+)
+
+
+@Bot.on_message(filters.private)
+async def start(bot, update):
+    text = BOT_TEXT.format(update.from_user.mention)
+    reply_markup = BOT_BUTTONS
+    await update.reply_text(
+        text=text, disable_web_page_preview=True, reply_markup=reply_markup
+    )
+
+print(
+    """
+Bot Contributed To {btname} And Started Started!!!
+"""
+)
+
+Bot.run()
