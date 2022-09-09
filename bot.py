2c5e6d335b91632ab07c4934e9317de60e1b2771
technoya <113179663+technoya@users.noreply.github.com>
2022-09-09 11:38:28 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,33 +14,6 @@ def start(update: Update, context: CallbackContext):
 		"Hello sir, Welcome to the Bot.Please write\
 		/help to see the commands available.")
 
-def help(update: Update, context: CallbackContext):
-	update.message.reply_text("""Available Commands :-
-	/youtube - To get the youtube URL
-	/linkedin - To get the LinkedIn profile URL
-	/gmail - To get gmail URL
-	/geeks - To get the GeeksforGeeks URL""")
-
-
-def gmail_url(update: Update, context: CallbackContext):
-	update.message.reply_text(
-	"BOT UNDER MAINTENCE !")
-
-
-def youtube_url(update: Update, context: CallbackContext):
-	update.message.reply_text("BOT UNDER MAINTENCE !")
-
-
-def linkedIn_url(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"LinkedIn URL => \
-		https://www.linkedin.com/in/dwaipayan-bandyopadhyay-007a/")
-
-
-def geeks_url(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"BOT UNDER MAINTENCE !!")
-
 
 def unknown(update: Update, context: CallbackContext):
 	update.message.reply_text(
@@ -53,11 +26,6 @@ def unknown_text(update: Update, context: CallbackContext):
 
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
-updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
-updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
-updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
 updater.dispatcher.add_handler(MessageHandler(
 	Filters.command, unknown)) # Filters out unknown commands
